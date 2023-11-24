c282b26a509059da548abd7f53fb6cc167895976
Purple_NightSky <jungseo102200@gmail.com>
2023-11-24 19:57:22 +0900

    2023.11.24 초기화면, 맵

---
+++ app/src/main/java/com/example/halla_navi/mapping.java
@@ -0,0 +1,15 @@
+package com.example.halla_navi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mapping extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapping);
+
+    }
+}
